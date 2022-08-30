60d8fd3173e8a561a2258d0a9fa6f01a593eaf52
Jack Delahunt <jdealhunt@gmail.com>
2022-08-30 20:58:21 +0100

    feat: added function code gen

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
 #include <algorithm>
+#include <cassert>
 #include <cctype>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <map>
 #include <memory>
 #include <string>
 #include <vector>
-#include <iostream>
 
 
 // The static variables will be used during code generation. context is an opaque object that owns a lot of
@@ -34,6 +41,10 @@ static std::unique_ptr<llvm::IRBuilder<>> builder;
 // why the codegen() method returns a raw Value*, rather than a unique_ptr<Value>.
 static std::unique_ptr<llvm::Module> module;
 
+static std::unique_ptr<llvm::legacy::FunctionPassManager> fpm;
+
+// In order to get per-function optimizations going, we need to set up a FunctionPassManager to hold and organize
+// the LLVM optimizations that we want to run
 struct Expression {
     virtual llvm::Value *codegen() = 0;
 };
@@ -78,20 +89,62 @@ struct NumberExpression : Expression {
     }
 };
 
+struct Statement {
+};
+
+struct FnStatement : Statement {
+    std::string name;
+    Expression *body;
+
+    FnStatement(std::string name, Expression *body) {
+        this->name = std::move(name);
+        this->body = body;
+    }
+
+    llvm::Function *codegen() {
+
+        // function type decl
+        llvm::FunctionType *ft = llvm::FunctionType::get(llvm::Type::getInt32Ty(*context), false);
+        llvm::Function *func = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, name, module.get());
+
+        // func body decl
+        llvm::BasicBlock *block = llvm::BasicBlock::Create(*context, name + "_body", func);
+        builder->SetInsertPoint(block);
+
+        auto ret = body->codegen();
+        builder->CreateRet(ret);
+        llvm::verifyFunction(*func);
+
+        fpm->run(*func);
+
+        return func;
+    }
+};
+
 int main() {
     context = std::make_unique<llvm::LLVMContext>();
     module = std::make_unique<llvm::Module>("llvm-module", *context);
     builder = std::make_unique<llvm::IRBuilder<>>(*context);
+    fpm = std::make_unique<llvm::legacy::FunctionPassManager>(module.get());
 
-    auto left = new NumberExpression(100);
-    auto right = new NumberExpression(200);
+    // Do simple "peephole" optimizations and bit-twiddling optzns.
+    //fpm->add(llvm::createInstructionCombiningPass());
+    // Reassociate expressions.
+    //fpm->add(llvm::createReassociatePass());
+    // Eliminate Common SubExpressions.
+    //fpm->add(llvm::createGVNPass());
+    // Simplify the control flow graph (deleting unreachable blocks, etc).
+    //fpm->add(llvm::createCFGSimplificationPass());
 
-    auto binary = new BinaryExpression(left, "+", right);
-    auto c = binary->codegen();
+    fpm->doInitialization();
 
+    auto top_function = FnStatement(
+    "main",
+    new BinaryExpression(new NumberExpression(100),"+",new NumberExpression(200))
+    );
 
+    top_function.codegen();
     module->print(llvm::outs(), nullptr);
-    c->print(llvm::outs());
 
     return 0;
 }
